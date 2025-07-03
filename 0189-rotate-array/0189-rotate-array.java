74e49c7934924f96d97dbf3c90a6b52e639547ea
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2025-07-03 16:35:12 +0900

    Time: 2 ms (26.75%), Space: 57.3 MB (65.93%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,21 +1,22 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        if (k == 0) {
-            return;
-        }
-        Deque<Integer> deq = new ArrayDeque<>();
+        k %= nums.length;
+        int[] result = new int[nums.length];
+        // result 형성
 
-        for (int i=0; i<nums.length; i++) {
-            deq.offer(nums[i]);
+        // k = 3
+        // 0 1 2 3 4 5 6
+        // 1 2 3 4 5 6 7
+
+        // 5 6 7 1 2 3 4
+
+        // 시작 (length - k + i) % length
+
+        for(int i = 0; i < nums.length; i++) {
+            result[i] = nums[(nums.length - k + i) % nums.length];
         }
 
-        for (int i=0; i<k; i++) {
-            int z = deq.pollLast();
-            deq.offerFirst(z);
-        }
-
-        for (int i=0; i<nums.length; i++) {
-            nums[i] = deq.poll();
-        }
+        // result 완성, 아래 펑션 헷갈림 -> 테스트 필요
+        System.arraycopy(result, 0, nums, 0, result.length);
     }
 }
