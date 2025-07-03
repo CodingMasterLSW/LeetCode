c1ef40e14d7119cd828fe435afb69c5968da6af9
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2025-07-03 16:43:53 +0900

    Time: 2 ms (26.75%), Space: 57.2 MB (85.8%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -2,21 +2,10 @@ class Solution {
     public void rotate(int[] nums, int k) {
         k %= nums.length;
         int[] result = new int[nums.length];
-        // result 형성
-
-        // k = 3
-        // 0 1 2 3 4 5 6
-        // 1 2 3 4 5 6 7
-
-        // 5 6 7 1 2 3 4
-
-        // 시작 (length - k + i) % length
-
+    
         for(int i = 0; i < nums.length; i++) {
             result[i] = nums[(nums.length - k + i) % nums.length];
         }
-
-        // result 완성, 아래 펑션 헷갈림 -> 테스트 필요
         System.arraycopy(result, 0, nums, 0, result.length);
     }
 }
